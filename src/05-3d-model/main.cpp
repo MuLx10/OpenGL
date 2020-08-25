3954f1804e0e8dfe3e01104f3416a0652d0eea8d
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2020-08-25 19:26:12 +0530

    .

--- src/05-3d-model/main.cpp
+++ src/05-3d-model/main.cpp
@@ -116,7 +116,7 @@ public:
 	}
 
 	GLuint GetProgramID(){return programID;}
-public:
+private:
 	GLuint vertexArrayID;
 	GLuint vertexBufferID, uvBufferID;
 	GLuint programID, textureID;
@@ -149,7 +149,7 @@ int InitWindow(){
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 640, 480, "Keyboard Controls", NULL, NULL);
+	window = glfwCreateWindow( 640, 480, "3d Model", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
